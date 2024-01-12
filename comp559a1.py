9d0a9b3d006c0b78737996b4ec93976a00db41b3
Robert-liu3 <robert.liu3@mail.mcgill.ca>
2024-01-12 16:45:21 -0500

    finished q1

--- comp559a1.py
+++ comp559a1.py
@@ -4,7 +4,7 @@ import igl
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--file", type=str, default="cube.obj")
-parser.add_argument("--density", type-float, default=1000, help="density in kg/m^3")
+parser.add_argument("--density", type=float, default=1000, help="density in kg/m^3")
 parser.add_argument("--scale", nargs=3, type=float, default=(1,1,1),help="scale the mesh")
 parser.add_argument("--translate", nargs=3, type=float, default=(0,0,0),help="translate (after scale)")
 parser.add_argument("--test", type=int, help="run a numbered unit test")
@@ -13,9 +13,45 @@ V, _, _, F, _, _ = igl.read_obj(args.file)
 V = V * args.scale
 V = V + args.translate
 
-x0, x1, x2 = symbols('x0 x1 x2')
-m1 = Matrix([[x0, x1, x2], [1, -1, -1], [-1,1,-1], [-1,-1,1]])
+
+# QUESTION 1
+x, y, z = symbols('x3 y3 z3')
+
+A = Matrix(V[F[0][0]])
+B = Matrix(V[F[0][1]])
+C = Matrix(V[F[0][2]])
+D = Matrix([x, y, z])
+
+# debugging print statements
+# print("first vector is: ", V[F[0][0]])
+# print("second vector is: ", V[F[0][1]])
+# print("third vector is: ", V[F[0][2]])
+
+vol_func = lambdify((x, y, z), abs(C.dot(A.cross(B))/6), "numpy")
+
+tetrahedron_volume = vol_func(0,0,0)
+
+mass = tetrahedron_volume * args.density
 
 
-vol_m1 = lambdify((x0, x1, x2), abs(m1.det()) / 6)
+
+# QUESTION 2
+
+
+
+if args.test == 1:
+
+    print("Volume of tetrahedron: ", tetrahedron_volume)
+
+
+    print("Mass of tetrahedron: ", mass)
+
+elif args.test == 2:
+
+    rho = symbols('rho', positive=True)
+
+    baycentric_func = lambdify((x, y, z), (C.dot(A.cross(B))/6), "numpy")
+
+    
+
 
